aa1a35a2d67d43ee452f1b532feb61f8b63ff2ee
Luis Ball <luis.ball@Luiss-MacBook-Pro.local>
2020-01-12 18:32:04 -0500

    add Road Runner shippment tracking support

---
+++ test/roadrunner.js
@@ -0,0 +1,32 @@
+/* globals before it describe */
+
+'use strict'
+
+const assert = require('assert')
+
+const prepare = require('./fixtures/prepare')
+const tracker = require('../')
+
+const courier = tracker.courier(tracker.COURIER.ROADRUNNER.CODE)
+
+describe(tracker.COURIER.ROADRUNNER.NAME, function () {
+  const deliveredNumber = 'DELIVEREDNM'
+
+  before(function () {
+    // @TODO add nock
+    prepare(courier, deliveredNumber)
+  })
+
+  it('delivered number', function (done) {
+    courier.trace(deliveredNumber, function (err, result) {
+      assert.equal(err, null)
+
+      assert.equal(deliveredNumber, result.number)
+      assert.equal(tracker.STATUS.DELIVERED, result.status)
+      assert.equal(tracker.COURIER.ROADRUNNER.CODE, result.courier.code)
+      assert.notEqual(result.checkpoints.length, 0)
+
+      done()
+    })
+  })
+})
